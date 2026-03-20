883d32fb16c3f27bf7c257cf7955c936635f95c0
delacruzrouel86-dev <delacruzrouel86@gmail.com>
2026-03-20 20:01:47 +0800

    Create seatwork8.java

---
+++ Semester 1 /seatwork8.java
@@ -0,0 +1,24 @@
+    public class seatwork8 {
+        public static void main(String[] args) {
+          
+        char grade = 3;
+         switch(grade) {
+         case 1 :
+          System.out.println("AMAZING!");
+            break;
+         case 2 :
+         case 3 :
+          System.out.println("NICE");
+            break;
+         case 4 :
+          System.out.println("LUCKY");
+         case 5 :
+          System.out.println("YOU SHOULD TRY AGAIN");
+            break; 
+          default:
+          System.out.println("Invalid grade");
+            }
+          System.out.println("Your grade is" + grade);
+
+            }
+    }
