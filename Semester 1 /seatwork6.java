0e8072fbc3f020185cfede153f716bb6963ae504
delacruzrouel86-dev <delacruzrouel86@gmail.com>
2026-03-20 19:56:57 +0800

    Create seatwork6.java

---
+++ Semester 1 /seatwork6.java
@@ -0,0 +1,13 @@
+public class seatwork6 {
+    public static void main(String[] args) {
+        int dice = 1;
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("OLATS");
+            } else {
+                System.out.println("PALDO!");
+            }
+            dice = dice + 1;
+        }
+    }
+}
