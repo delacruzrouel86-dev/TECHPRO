64dcdc79c27534bbe860a13592c42c505563deaf
delacruzrouel86-dev <delacruzrouel86@gmail.com>
2026-03-20 20:04:00 +0800

    Create seatwork9.java

---
+++ Semester 1 /seatwork9.java
@@ -0,0 +1,12 @@
+public class seatwork9 {
+    public static void main(String[] args) {
+        
+        int[] numbers = {10, 20, 30, 40, 50};
+        int index = 0;
+
+        while (index < numbers.length) {
+            System.out.println("Value of item: " + numbers[index]);
+            index++;
+        }
+    }  
+}
