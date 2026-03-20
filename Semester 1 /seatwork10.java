42a7169bed785f7f9e5c159c9c38800c5afad7e5
delacruzrouel86-dev <delacruzrouel86@gmail.com>
2026-03-20 20:14:21 +0800

    Create seatwork10.java

---
+++ Semester 1 /seatwork10.java
@@ -0,0 +1,11 @@
+public class seatwork10 {
+ public static void main(String[] args) {
+
+  for(int x = 10; x < 20; x = x + 1) {
+
+        System.out.println("value of x: " + x );
+        System.out.println("\n");
+
+      }
+    }    
+}
