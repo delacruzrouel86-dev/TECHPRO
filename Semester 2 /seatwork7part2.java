7df431041a11aac95029022fa88c22242ae34e34
delacruzrouel86-dev <delacruzrouel86@gmail.com>
2026-03-21 07:17:53 +0800

    Create seatwork7part2.java

---
+++ Semester 2 /seatwork7part2.java
@@ -0,0 +1,25 @@
+class PasswordException extends Exception {
+	public PasswordException(String message) {
+		super(message);
+	}
+}
+
+public class seatwork7part2 {
+	
+	static void checkPassword(String password) throws PasswordException {
+		if (password.length() < 8) {
+			throw new PasswordException("Password must contain 8 characters or above");
+		} else { 
+		System.out.println("Password Accepted");
+		}
+	}
+	
+	public static void main (String[]args) {
+		try {
+			checkPassword("IHateNikkers");
+		} catch (PasswordException e) {
+			System.out.println("User-Defined Exception:" + e.getMessage());
+		}
+    
+    }
+}
